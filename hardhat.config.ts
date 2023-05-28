3abb25fb509df20c483498b7c7ac670ea21777e6
Zeroupper <mikloshavlik@gmail.com>
2023-05-28 14:54:17 +0200

    fix: upgrade solidity version

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,6 @@
+import "@nomicfoundation/hardhat-toolbox";
 import '@openzeppelin/hardhat-upgrades';
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
