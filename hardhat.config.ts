43ac4bfe2b04649673ef7b944e70438c58967831
Zeroupper <mikloshavlik@gmail.com>
2023-06-06 20:12:49 +0200

    fix: add tasks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,6 @@
 import "@nomicfoundation/hardhat-toolbox";
 import '@openzeppelin/hardhat-upgrades';
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, task } from "hardhat/config";
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -13,5 +13,21 @@ const config: HardhatUserConfig = {
     },
   } 
 };
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+task("balance", "Prints an account's balance")
+  .addParam("account", "The account's address")
+  .setAction(async (taskArgs, hre) => {
+    const balance = await hre.ethers.provider.getBalance(taskArgs.account);
+
+    console.log(hre.ethers.utils.formatEther(balance), "ETH");
+});
 
 export default config;
